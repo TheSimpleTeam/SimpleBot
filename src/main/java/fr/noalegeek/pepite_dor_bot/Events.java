ce2019a8d4d13eff6cb43dc5bcc682a537ec804f
NoaLeGeek68 <noacraft68@gmail.com>
2021-05-26 18:46:45 +0200

    !perfectnumber Release

--- src/main/java/fr/noalegeek/pepite_dor_bot/Events.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/Events.java
@@ -18,22 +18,33 @@ public class Events extends ListenerAdapter {
         //TODO Faire la commande !perfectnumber
         if(event.getAuthor().isBot()) return;
         if (event.getChannel().getId().equalsIgnoreCase("846351256176033842")) {
-            switch(event.getMessage().getContentRaw()){
+            switch(args[0]){
                 case "!e":
                     event.getChannel().sendMessage("E").queue();
                     break;
                 case "!perfectnumber":
                     if(args.length == 2) {
                         try {
+                            int addNumbers = 0;
                             int chooseNumber = Integer.parseInt(args[1]);
-                            event.getMessage().reply("okay").queue();
+                            event.getMessage().reply("**[**"+event.getAuthor().getAsMention()+"**]** Vérification en cours...").queue();
+                            for(int i = 1;i < chooseNumber;i++){
+                                if(chooseNumber % i == 0){
+                                    addNumbers += i;
+                                }
+                            }
+                            if(addNumbers == chooseNumber){
+                                event.getChannel().sendMessage("**[**"+event.getAuthor().getAsMention()+"**]** "+chooseNumber+" est un nombre parfait.").queue();
+                            } else {
+                                event.getChannel().sendMessage("**[**"+event.getAuthor().getAsMention()+"**]** "+chooseNumber+" n'est pas un nombre parfait.").queue();
+                            }
                         } catch (NumberFormatException numberFormatException){
                             event.getMessage().reply("**[**"+event.getAuthor().getAsMention()+"**]** Le nombre spécifié n'est pas un nombre entier.").queue();
                         }
                     } else if(args.length < 2){
                         event.getMessage().reply("**[**"+event.getAuthor().getAsMention()+"**]** Syntaxe de la commande !perfectnumber : ``!perfectnumber <nombre>``. Le nombre spécifié doit être un nombre entier.").queue();
                     } else {
-                        event.getMessage().reply("**[**" + event.getAuthor().getAsMention() + "**]**").queue();
+                        event.getMessage().reply("**[**" + event.getAuthor().getAsMention() + "**]** Vous devez mettre seulement un nombre entier.").queue();
                     }
                     break;
                 case "!test":
