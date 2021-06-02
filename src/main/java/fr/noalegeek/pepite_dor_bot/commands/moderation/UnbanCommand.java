b9e74e7f82120db452168a7f3dd4417bd91be576
minemobs <issahakim94@gmail.com>
2021-06-02 14:33:36 +0200

    Added unban command

---
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/moderation/UnbanCommand.java
@@ -0,0 +1,35 @@
+package fr.noalegeek.pepite_dor_bot.commands.moderation;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import fr.noalegeek.pepite_dor_bot.utils.helpers.MessageHelper;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.User;
+
+public class UnbanCommand extends Command {
+
+    public UnbanCommand() {
+        this.name = "unban";
+        this.arguments = "<mention>";
+        this.userPermissions = new Permission[]{Permission.BAN_MEMBERS};
+        this.botPermissions = new Permission[]{Permission.BAN_MEMBERS};
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        String[] args = event.getArgs().split(" ");
+        if (event.getAuthor().isBot()) return;
+        if (event.getArgs().length() == 1) {
+            event.replyError(MessageHelper.formattedMention(event.getAuthor())+"Syntaxe de la commande !ban : ``!ban <user ou userID> <temps en jours> [raison]``.");
+            return;
+        }
+        User target = event.getMessage().getMentionedUsers().get(0);
+        if (target == null) {
+            event.replyError(MessageHelper.formattedMention(event.getAuthor())+"Vous devez spécifié un membre existant.");
+            return;
+        }
+        event.getGuild().unban(target).queue(unused ->
+                event.replySuccess("L'utilisateur " + target.getName() + " à bien été unban."));
+
+    }
+}
