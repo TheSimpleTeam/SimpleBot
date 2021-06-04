281fc78ae0d517b5cfc3b0309d3c1b4487ce3b9c
minemobs <issahakim94@gmail.com>
2021-06-04 21:08:34 +0200

    NOA JVAIS VENIR CHEZ TOI T4ETRIPER

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/SuggestionCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/SuggestionCommand.java
@@ -7,7 +7,6 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.awt.*;
-import java.time.Instant;
 import java.time.OffsetDateTime;
 
 public class SuggestionCommand extends BotCommand {
