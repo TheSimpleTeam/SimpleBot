5934f9f76f56a714d833757cda67233c31581c30
NoaLeGeek68 <noacraft68@gmail.com>
2021-06-06 16:41:17 +0200

    Added error when message are too old (!purge Command) (minemobs tu devrais vérifier si cela est bien)

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/moderation/PurgeCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/moderation/PurgeCommand.java
@@ -23,6 +23,7 @@ public class PurgeCommand extends Command {
     protected void execute(CommandEvent event) {
         String[] args = event.getArgs().split(" \\s+");
         final AuditableRestAction<Void> deleteMessage = event.getMessage().delete();
+        boolean isMessageOld = false;
         if(event.getArgs().isEmpty()) {
             event.replyError(MessageHelper.syntaxError(event.getAuthor(), this)+"Le nombre de messages à spécifier doit se situer entre 1 et 100.");
             return;
@@ -32,7 +33,7 @@ public class PurgeCommand extends Command {
             if(Integer.parseInt(args[0]) < 0) {
                 event.replyWarning("La valeur minimum est de 1.");
             }else if(Integer.parseInt(args[0]) > 100) {
-                event.replyWarning("La valeur maximale est de 100.");
+                event.replyWarning("La valeur maximale est de 100.\n");
                 clearMessages = 100;
             } else {
                 clearMessages = Integer.parseInt(args[0]);
@@ -42,9 +43,19 @@ public class PurgeCommand extends Command {
             return;
         }
         int finalClearMessages = clearMessages;
-        event.getTextChannel().getHistory().retrievePast(clearMessages + 1).queue(messages -> deleteMessage.queue(unused -> {
-            event.getTextChannel().purgeMessages(messages);
-            event.replySuccess(finalClearMessages + " ont bien été supprimés", message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
-        }));
+        try {
+            for(int i = 1;i < finalClearMessages;i+=0){
+                event.getTextChannel().getHistory().retrievePast(i).queue(messages -> deleteMessage.queue(unused -> {
+                    event.getTextChannel().purgeMessages(messages);
+
+                }));
+            }
+            event.replySuccess(finalClearMessages + " messages ont bien été supprimés.", messageSucces -> messageSucces.delete().queueAfter(5000, TimeUnit.SECONDS));
+            if(isMessageOld){
+                event.getChannel().sendMessage(":warning: Les messages datant de plus 2 semaines n'ont pas pu être supprimé !").queue();
+            }
+        } catch (IllegalArgumentException ex){
+            isMessageOld = true;
+        }
     }
 }
