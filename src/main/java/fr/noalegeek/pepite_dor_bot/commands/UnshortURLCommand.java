3e545ca235c39b23504f008ddd754b1571bfd252
minemobs <issahakim94@gmail.com>
2021-06-11 15:04:21 +0200

    Added UnShortURLCommand (I will rework on that later)

---
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/UnshortURLCommand.java
@@ -0,0 +1,46 @@
+package fr.noalegeek.pepite_dor_bot.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.shekhargulati.urlcleaner.UrlCleaner;
+import fr.noalegeek.pepite_dor_bot.Main;
+import fr.noalegeek.pepite_dor_bot.utils.helpers.MessageHelper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class UnshortURLCommand extends Command {
+
+    public UnshortURLCommand() {
+        this.name = "unshorturl";
+        this.aliases = new String[]{"usu"};
+        this.arguments = "<URL>";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        String[] args = event.getArgs().split("\\s");
+        try {
+            event.replySuccess(String.format("`%s`", getURL(args[0])));
+        } catch (IOException e) {
+            MessageHelper.sendError(e, event);
+            return;
+        }
+    }
+
+    public String getURL(String _url) throws IOException {
+        //Idk how i should name it
+        String url = UrlCleaner.unshortenUrl(_url);
+        String[] urls = url.replace("https://", "").replace("http://", "").split("/");
+        if(urls[0].equalsIgnoreCase("preview.tinyurl.com")) {
+            Document doc = Jsoup.connect(_url).get();
+            Elements redirectURL = doc.getElementById("contentcontainer").getElementsByAttribute("blockquote");
+            Main.LOGGER.info(String.valueOf(redirectURL.size()));
+            String docText = doc.body().text();
+            return docText.substring(docText.indexOf("to: "), docText.indexOf(" Proceed")).replace("to: ", "").replace(" Proceed", "");
+        }
+        return url;
+    }
+}
--- src/main/java/fr/noalegeek/pepite_dor_bot/utils/helpers/MessageHelper.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/utils/helpers/MessageHelper.java
@@ -1,6 +1,7 @@
 package fr.noalegeek.pepite_dor_bot.utils.helpers;
 
 import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import fr.noalegeek.pepite_dor_bot.Main;
 import net.dv8tion.jda.api.entities.User;
 
@@ -8,9 +9,11 @@ import java.time.OffsetDateTime;
 import java.util.Locale;
 
 public class MessageHelper {
+
     public static String getTag(final User user) {
         return user.getName() + "#" + user.getDiscriminator();
     }
+
     public static String formatDate(OffsetDateTime date) {
         int day = date.getDayOfMonth();
         int month = date.getMonthValue();
@@ -21,9 +24,11 @@ public class MessageHelper {
         }
         return day + "/" + month + "/" + year;
     }
+
     public static String formattedMention(User user) {
         return String.format("**[**%s**]** ", user.getAsMention());
     }
+
     public static String syntaxError(User user, Command command) {
         String syntaxMessage = MessageHelper.formattedMention(user) + "Syntaxe de la commande " + Main.getInfos().prefix + command.getName() + " : `" + Main.getInfos().prefix + command.getName()
                 + " " + command.getArguments() + "`.\n";
@@ -32,6 +37,7 @@ public class MessageHelper {
         }
         return syntaxMessage;
     }
+
     public static String formatEnum(String name) {
         StringBuilder builder = new StringBuilder();
         char[] nameChar = name.toCharArray();
@@ -44,6 +50,7 @@ public class MessageHelper {
         }
         return builder.toString();
     }
+
     public static String formatEnum(Enum<?> _enum) {
         StringBuilder builder = new StringBuilder();
         char[] nameArray = _enum.name().toLowerCase(Locale.ROOT).toCharArray();
@@ -55,5 +62,10 @@ public class MessageHelper {
             }
         }
         return builder.toString();
+    }
+
+    public static void sendError(Exception ex, CommandEvent event) {
+        event.replyError("Une erreur est survenue. Veuillez contacter les développeurs\n" + ex.getMessage());
+        Main.LOGGER.severe(ex.getMessage());
     }
 }
