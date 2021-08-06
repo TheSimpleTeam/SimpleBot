6a3a62cc3544e3950b9379d8b43a0a12c7f7019b
NoaLeGeek68 <noacraft68@gmail.com>
2021-08-06 17:34:01 +0200

    Make the code of ChannelMemberCommand class readable

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/ChannelMemberCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/ChannelMemberCommand.java
@@ -6,7 +6,9 @@ import fr.noalegeek.pepite_dor_bot.Main;
 import fr.noalegeek.pepite_dor_bot.enums.CommandCategories;
 import fr.noalegeek.pepite_dor_bot.utils.helpers.MessageHelper;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.User;
 
 import java.util.Locale;
 
@@ -26,84 +28,82 @@ public class ChannelMemberCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        if(event.getAuthor().isBot()) return;
+        User author = event.getAuthor();
+        if(author.isBot()) return;
         String[] args = event.getArgs().split("\\s+");
+        String syntaxError = "Les arguments disponibles sont **join** et **remove**.\n" +
+                "- **join** définira le salon où les annonces de bienvenues apparaîtront.\n" +
+                "- **remove** définira le salon où les annonces de départs apparaîtront.\n" +
+                "Après ces arguments, les arguments disponibles sont **identifiant/mention du salon**, **reset** et **this**.\n" +
+                "- **identifiant/mention du salon** définira le salon grâce à son indentifiant ou sa mention.\n" +
+                "- **reset** réinitialisera le salon qui a été configuré\n" +
+                "- **this** définira le salon où a été fait la commande.";
         if(args.length != 2){
-            event.replyError(MessageHelper.syntaxError(event.getAuthor(),this)+
-                    "Les arguments disponibles sont **join** et **remove**.\n" +
-                    "- **join** définira le salon où les annonces de bienvenues apparaîtront.\n" +
-                    "- **remove** définira le salon où les annonces de départs apparaîtront.\n" +
-                    "Après ces arguments, les arguments disponibles sont **identifiant/mention du salon**, **reset** et **this**.\n" +
-                    "- **identifiant/mention du salon** définira le salon grâce à son indentifiant ou sa mention.\n" +
-                    "- **reset** réinitialisera le salon qui a été configuré\n" +
-                    "- **this** définira le salon où a été fait la commande.");
+            event.replyError(MessageHelper.syntaxError(author,this) + syntaxError);
             return;
         }
-        GuildChannel channelMember = event.getGuild().getGuildChannelById(args[1].replace("<#", "").replace(">", ""));
+        Guild guild = event.getGuild();
+        GuildChannel channelMember = guild.getGuildChannelById(args[1].replaceAll("\\D+", ""));
         if(channelMember == null){
             event.replyError("Ce salon n'existe pas.");
             return;
         }
+        
         switch (args[0].toLowerCase(Locale.ROOT)){
             case "join":
+                String channelMemberId = Main.getServerConfig().channelMemberJoin.get(guild.getId());
                 switch (args[1].toLowerCase(Locale.ROOT)){
                     case "reset":
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberJoin.get(event.getGuild().getId())) == null){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon n'a pas été configuré donc vous ne pouvez pas le réinitialiser.");
+                        if(guild.getGuildChannelById(channelMemberId) == null){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon n'a pas été configuré donc vous ne pouvez pas le réinitialiser.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberJoin.remove(event.getGuild().getId());
+                        Main.getServerConfig().channelMemberJoin.remove(guild.getId());
                         break;
                     case "this":
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberJoin.get(event.getGuild().getId())).equals(event.getGuild().getGuildChannelById(event.getChannel().getId()))){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
+                        if(guild.getGuildChannelById(channelMemberId).equals(guild.getGuildChannelById(event.getChannel().getId()))){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberJoin.put(event.getGuild().getId(),channelMember.getId());
+                        Main.getServerConfig().channelMemberJoin.put(guild.getId(),channelMember.getId());
                         break;
                     default:
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberJoin.get(event.getGuild().getId())) == null && event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberJoin.get(event.getGuild().getId())).equals(channelMember)){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
+                        if(guild.getGuildChannelById(channelMemberId) == null && guild.getGuildChannelById(channelMemberId).equals(channelMember)){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberJoin.put(event.getGuild().getId(),channelMember.getId());
+                        Main.getServerConfig().channelMemberJoin.put(guild.getId(),channelMember.getId());
                         break;
                 }
                 break;
             case "remove":
+                channelMemberId = Main.getServerConfig().channelMemberRemove.get(guild.getId());
                 switch (args[1].toLowerCase(Locale.ROOT)){
                     case "reset":
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberRemove.get(event.getGuild().getId())) == null){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon n'a pas été configuré donc vous ne pouvez pas le réinitialiser.");
+                        if(guild.getGuildChannelById(channelMemberId) == null){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon n'a pas été configuré donc vous ne pouvez pas le réinitialiser.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberRemove.remove(event.getGuild().getId());
+                        Main.getServerConfig().channelMemberRemove.remove(guild.getId());
                         break;
                     case "this":
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberRemove.get(event.getGuild().getId())).equals(event.getGuild().getGuildChannelById(event.getChannel().getId()))){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
+                        if(guild.getGuildChannelById(channelMemberId).equals(guild.getGuildChannelById(event.getChannel().getId()))){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberRemove.put(event.getGuild().getId(),channelMember.getId());
+                        Main.getServerConfig().channelMemberRemove.put(guild.getId(),channelMember.getId());
                         break;
                     default:
-                        if(event.getGuild().getGuildChannelById(Main.getServerConfig().channelMemberRemove.get(event.getGuild().getId())).equals(channelMember)){
-                            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
+                        if(guild.getGuildChannelById(channelMemberId).equals(channelMember)){
+                            event.replyError(MessageHelper.formattedMention(author) + "Le salon que vous voulez changer pour les messages de bienvenue est le même que celui configuré actuellement.");
                             return;
                         }
-                        Main.getServerConfig().channelMemberRemove.put(event.getGuild().getId(),channelMember.getId());
+                        Main.getServerConfig().channelMemberRemove.put(guild.getId(),channelMember.getId());
                         break;
                 }
                 break;
             default:
-                event.replyError(MessageHelper.syntaxError(event.getAuthor(),this)+
-                        "Les arguments disponibles sont **join** et **remove**.\n" +
-                        "- **join** définira le salon où les annonces de bienvenues apparaîtront.\n" +
-                        "- **remove** définira le salon où les annonces de départs apparaîtront.\n" +
-                        "Après ces arguments, les arguments disponibles sont **identifiant/mention du salon**, **reset** et **this**.\n" +
-                        "- **identifiant/mention du salon** définira le salon grâce à son indentifiant ou sa mention.\n" +
-                        "- **reset** réinitialisera le salon qui a été configuré\n" +
-                        "- **this** définira le salon où a été fait la commande.");
+                event.replyError(MessageHelper.syntaxError(author,this) + syntaxError);
         }
         event.replySuccess("Le salon " + channelMember.getAsMention() + " a bien été défini.");
     }
