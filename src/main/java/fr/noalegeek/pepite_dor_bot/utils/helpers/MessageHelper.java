43edc4fdc02b91b33d4b754db4cfd66013d18c1d
NoaLeGeek68 <noacraft68@gmail.com>
2021-08-14 20:05:41 +0200

    Changed the name of sendTranslatedMessage for translateMessage. Changed parameter User of syntaxError function for CommandEvent

--- src/main/java/fr/noalegeek/pepite_dor_bot/utils/helpers/MessageHelper.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/utils/helpers/MessageHelper.java
@@ -6,12 +6,9 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import fr.noalegeek.pepite_dor_bot.Main;
 import net.dv8tion.jda.api.entities.User;
 
+import javax.annotation.Nullable;
 import java.time.OffsetDateTime;
-<<<<<<<<< Temporary merge branch 1
-=========
-import java.util.Locale;
 import java.util.Optional;
->>>>>>>>> Temporary merge branch 2
 
 public class MessageHelper {
 
@@ -23,13 +20,14 @@ public class MessageHelper {
         return String.format("**[**%s**]** ", user.getAsMention());
     }
 
-    public static String syntaxError(User user, Command command) {
-        String syntaxMessage = formattedMention(user) + "Syntaxe de la commande " + Main.getInfos().prefix + command.getName() + " : `" + Main.getInfos().prefix + command.getName();
+    public static String syntaxError(CommandEvent event, Command command) {
+        String syntaxMessage = MessageHelper.formattedMention(event.getAuthor()) + "Syntaxe de la commande " + Main.getInfos().prefix + command.getName() + " : `" +
+                Main.getInfos().prefix + command.getName();
         if(!command.getArguments().isEmpty()) syntaxMessage += " " + command.getArguments() + "`.\n";
         else syntaxMessage += "`.\n";
         if(!command.getHelp().isEmpty()) syntaxMessage += command.getHelp() + "\n";
-        if(!command.getExample().isEmpty()) syntaxMessage += "Par exemple : `" + Main.getInfos().prefix+command.getName() + " " + command.getExample() + "`.\n";
-        return syntaxMessage + "\n";
+        if(!command.getExample().isEmpty()) syntaxMessage += "Par exemple : `"+Main.getInfos().prefix+command.getName()+" "+command.getExample()+"`.\n";
+        return syntaxMessage;
     }
 
     public static void sendError(Exception ex, CommandEvent event) {
@@ -42,7 +40,7 @@ public class MessageHelper {
         int month = date.getMonthValue();
         int year = date.getYear();
         if(month < 10){
-            String strMonth = "0" + month;
+            String strMonth = "0"+month;
             return day + "/" + strMonth + "/" + year;
         }
         return day + "/" + month + "/" + year;
@@ -55,16 +53,13 @@ public class MessageHelper {
      * @return the translated value
      * @throws NullPointerException if the key does not exist in any localization files.
      */
-    @Nullable
-    public static String sendTranslatedMessage(String key, String guildID) {
+    public static String translateMessage(String key, String guildID) {
         String lang = Main.getServerConfig().language.getOrDefault(guildID, "en");
         Optional<JsonElement> s = Optional.ofNullable(Main.getLocalizations().get(lang).get(key));
         if(s.isPresent()) return s.get().getAsString();
-
         if (Main.getLocalizations().get("en").get(key) == null) {
             throw new NullPointerException("This key does not exist in any localization file !");
         }
         return Main.getLocalizations().get("en").get(key).getAsString();
-
     }
 }
