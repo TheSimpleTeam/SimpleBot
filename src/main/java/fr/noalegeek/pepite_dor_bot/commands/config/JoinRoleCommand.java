8e2830b7302f18d27e9f018712ea106182079110
NoaLeGeek68 <noacraft68@gmail.com>
2021-08-14 19:58:31 +0200

    Added translated texts for JoinRoleCommand and LocalizationCommand

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/JoinRoleCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/JoinRoleCommand.java
@@ -17,8 +17,8 @@ public class JoinRoleCommand extends Command {
     public JoinRoleCommand() {
         this.name = "joinrole";
         this.aliases = new String[]{"jr","joinr","jrole"};
-        this.help = "Défini le rôle que va avoir un utilisateur lors de son arrivée sur un serveur.";
-        this.arguments = "<identifiant/mention du role|reset>";
+        this.help = "help.joinRole";
+        this.arguments = "arguments.joinRole";
         this.category = CommandCategories.CONFIG.category;
         this.cooldown = 5;
         this.example = "660083059089080321";
@@ -29,36 +29,36 @@ public class JoinRoleCommand extends Command {
     protected void execute(CommandEvent event) {
         if(event.getAuthor().isBot()) return;
         if(!event.getMember().isOwner()){
-            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("error.commands.notowner", event.getGuild().getId()));
+            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("error.commands.notOwner", event.getGuild().getId()));
             return;
         }
         String[] args = event.getArgs().split(" \\s+");
         if (args.length != 2) {
-            event.replyError(MessageHelper.syntaxError(event.getAuthor(),this) + MessageHelper.sendTranslatedMessage("syntax.joinrole", event.getGuild().getId()));
+            event.replyError(MessageHelper.syntaxError(event,this) + MessageHelper.translateMessage("syntax.joinrole", event.getGuild().getId()));
             return;
         }
         Role joinRole = event.getGuild().getRoleById(args[0].replaceAll("\\D+",""));
         if (joinRole == null) {
-            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("error.joinrole.rolenull", event.getGuild().getId()));
+            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("error.joinrole.roleNull", event.getGuild().getId()));
             return;
         } else if (joinRole.isManaged()) {
-            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("error.joinrole.rolemanaged", event.getGuild().getId()));
+            event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("error.joinrole.roleManaged", event.getGuild().getId()));
             return;
         }
         String joinRoleId = Main.getServerConfig().guildJoinRole.get(event.getGuild().getId());
         if(args[0].equalsIgnoreCase("reset")){
             if(joinRoleId == null){
-                event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("error.joinrole.notconfigured", event.getGuild().getId()));
+                event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("error.joinrole.notConfigured", event.getGuild().getId()));
                 return;
             }
             Main.getServerConfig().guildJoinRole.remove(event.getGuild().getId());
         } else {
             if(joinRoleId.equals(joinRole.getId())){
-                event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("error.joinrole.sameasconfigured", event.getGuild().getId()));
+                event.replyError(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("error.joinrole.sameAsConfigured", event.getGuild().getId()));
                 return;
             }
             Main.getServerConfig().guildJoinRole.put(event.getGuild().getId(), joinRole.getId());
-            event.replySuccess(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.sendTranslatedMessage("success.joinrole.configured", event.getGuild().getId()));
+            event.replySuccess(MessageHelper.formattedMention(event.getAuthor()) + MessageHelper.translateMessage("success.joinrole.configured", event.getGuild().getId()));
         }
     }
 }
--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/LocalizationCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/config/LocalizationCommand.java
@@ -3,24 +3,31 @@ package fr.noalegeek.pepite_dor_bot.commands.config;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import fr.noalegeek.pepite_dor_bot.Main;
+import fr.noalegeek.pepite_dor_bot.enums.CommandCategories;
 import fr.noalegeek.pepite_dor_bot.utils.helpers.MessageHelper;
 import net.dv8tion.jda.api.Permission;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class LocalizationCommand extends Command {
 
     public LocalizationCommand() {
         this.name = "localization";
-        this.aliases = new String[]{"clc"};
-        this.userPermissions = new Permission[]{Permission.ADMINISTRATOR};
+        this.cooldown = 5;
+        this.help = "help.localization";
+        this.example = "en";
+        this.aliases = new String[]{"l","lo","local","loc"};
+        this.arguments = "arguments.localization";
+        this.category = CommandCategories.CONFIG.category;
+        this.guildOnly = true;
     }
 
     @Override
     protected void execute(CommandEvent event) {
         String[] args = event.getArgs().split("\\s+");
         if(args.length == 0) {
-            MessageHelper.syntaxError(event.getAuthor(), this);
+            MessageHelper.syntaxError(event, this);
             return;
         }
         if(Arrays.stream(Main.getLangs()).noneMatch(s -> s.equalsIgnoreCase(args[0]))) {
@@ -28,6 +35,6 @@ public class LocalizationCommand extends Command {
             return;
         }
         Main.getServerConfig().language.put(event.getGuild().getId(), args[0]);
-        event.replySuccess(String.format(MessageHelper.sendTranslatedMessage("msg.languageconfig", event.getGuild().getId()), ":flag_" + args[0] + ':'));
+        event.replySuccess(String.format(MessageHelper.translateMessage("success.localization.configured", event.getGuild().getId()), ":flag_" + args[0] + ':'));
     }
 }
