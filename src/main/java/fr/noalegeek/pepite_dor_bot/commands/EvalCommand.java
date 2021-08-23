35e1765f20b6636f9bf3b80a58497f4a2212d202
minemobs <issahakim94@gmail.com>
2021-08-23 23:53:18 +0200

    Added EvalCommand

---
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/EvalCommand.java
@@ -0,0 +1,52 @@
+package fr.noalegeek.pepite_dor_bot.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import fr.noalegeek.pepite_dor_bot.utils.helpers.MessageHelper;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.StringWriter;
+
+public class EvalCommand extends Command {
+
+    private final ScriptEngine engine;
+    private final StringWriter writer;
+    private final ScriptContext context;
+
+    public EvalCommand() {
+        this.name = "eval";
+        this.ownerCommand = true;
+        this.hidden = true;
+        this.guildOnly = true;
+        System.setProperty("polyglot.js.nashorn-compat", "true");
+        this.engine = new ScriptEngineManager().getEngineByName("graal.js");
+        this.context = engine.getContext();
+        this.writer = new StringWriter();
+        context.setWriter(writer);
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        String args = event.getArgs().replaceAll("([^(]+?)\\s*->", "function($1)");
+        try {
+            engine.put("event", event);
+            engine.put("message", event.getMessage());
+            engine.put("channel", event.getChannel());
+            engine.put("args", event.getArgs().split("\\s+"));
+            engine.put("api", event.getJDA());
+            engine.put("guild", event.getGuild());
+            engine.put("member", event.getMember());
+            engine.eval(args);
+
+            if(writer.toString() == null){
+                event.replySuccess("Evaluated Successfully");
+            }else{
+                event.replySuccess("Evaluated Successfully:\n```\n" + writer + " ```");
+            }
+        } catch (Exception ex) {
+            MessageHelper.sendError(ex, event);
+        }
+    }
+}
