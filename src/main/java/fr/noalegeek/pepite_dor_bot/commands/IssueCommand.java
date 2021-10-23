155f490581f2cb0a83b05817a9d152ccdef2589a
minemobs <issahakim94@gmail.com>
2021-10-23 18:16:35 +0200

    Added parameters for the IssueCommand

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/IssueCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/IssueCommand.java
@@ -26,12 +26,12 @@ package fr.noalegeek.pepite_dor_bot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import fr.noalegeek.pepite_dor_bot.GithubInfo;
 import fr.noalegeek.pepite_dor_bot.Main;
 import fr.noalegeek.pepite_dor_bot.commands.annotations.RequireConfig;
 import fr.noalegeek.pepite_dor_bot.utils.MessageHelper;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
+import org.apache.commons.cli.*;
+import org.kohsuke.github.*;
 
 import java.io.IOException;
 
@@ -48,10 +48,15 @@ public class IssueCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        if(event.getArgs().split("\\s+").length == 0) {
+        String[] args = event.getArgs().split("\\s");
+        Options options = new Options();
+        CommandLineParser parser = new DefaultParser();
+
+        if(args.length == 0) {
             MessageHelper.syntaxError(event, this);
             return;
         }
+
         try {
             this.gh.checkApiUrlValidity();
         } catch (IOException ex) {
@@ -59,17 +64,39 @@ public class IssueCommand extends Command {
             return;
         }
         try {
-            GHRepository repo = this.gh.getRepositoryById(370083508);
-            repo.createIssue("Automatic Issue").body(String.format("""
+            addOptions(options);
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.getOptions().length != 0 && !cmd.hasOption("body")) {
+                event.replyError("This command require parameter **--body** because you use parameters !");
+                return;
+            }
+            GHRepository repo = this.gh.getRepositoryById(GithubInfo.REPOSITORY_ID.id);
+            GHOrganization org = this.gh.getOrganization(GithubInfo.ORGANISATION_ID.name);
+            GHIssueBuilder builder = repo.createIssue(String.join(" ", getOrDefault(cmd, "title", "Automatic Issue"))).body(String.format("""
                 This issue has been made by %s on %s
                 
                 ----
                 
                 %s
-                """, event.getAuthor().getName(), event.getGuild().getName(), event.getArgs())).create();
-        } catch (IOException exception) {
+                """, event.getAuthor().getName(), event.getGuild().getName(), String.join(" ", getOrDefault(cmd, "body", event.getArgs()))));
+            builder.create();
+        } catch (IOException | ParseException exception) {
             MessageHelper.sendError(exception, event);
         }
+    }
 
+    private void addOptions(Options options) {
+        Option body = new Option("b", "body", true, "Add body to the issue");
+        Option title = new Option("t", "title", true, "Add title to the issue");
+
+        body.setArgs(Option.UNLIMITED_VALUES);
+        title.setArgs(Option.UNLIMITED_VALUES);
+
+        options.addOption(body);
+        options.addOption(title);
+    }
+
+    private String[] getOrDefault(CommandLine options, String option, String o) {
+        return options.hasOption(option) ? options.getOptionValues(option) : new String[]{o};
     }
 }
