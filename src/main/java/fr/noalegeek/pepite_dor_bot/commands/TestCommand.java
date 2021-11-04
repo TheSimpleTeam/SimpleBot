f1abec76cf7caea75495e786d55f74f77d6462d7
minemobs <issahakim94@gmail.com>
2021-11-04 12:49:42 +0100

    Updated jda-chewtils and gson

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/TestCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/TestCommand.java
@@ -13,19 +13,11 @@ public class TestCommand extends Command {
         this.name = "test";
         this.hidden = true;
         this.aliases = new String[]{"t", "te", "tes"};
-        this.ownerCommand = true;
+        this.guildOwnerCommand = true;
     }
 
     @Override
     protected void execute(CommandEvent event) {
-        StringBuilder stringBuilder = new StringBuilder().append("\"");
-        for(String part1 : new String[]{"temp", "tem", "te", "t"}){
-            for(String part2 : new String[]{"b", "ban", "ba"}){
-                if(!(part1 + part2).equals("tempban")){
-                    stringBuilder.append(part1).append(part2).append("\",\"");
-                }
-            }
-        }
-        event.reply(stringBuilder.toString());
+        event.reply("Caca");
     }
 }
