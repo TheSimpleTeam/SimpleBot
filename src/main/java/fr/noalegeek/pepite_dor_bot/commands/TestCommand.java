f1bb8032750471f09b18f379ad740e56640018ef
NoaLeGeek68 <noacraft68@gmail.com>
2021-12-22 21:31:14 +0100

    informations.config key done (French only)

--- src/main/java/fr/noalegeek/pepite_dor_bot/commands/TestCommand.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/commands/TestCommand.java
@@ -38,6 +38,76 @@ public class TestCommand extends Command {
                 }
             }
         }*/
+        System.out.println("""
+                Les arguments disponibles sont :
+                
+                - **channelmember** :
+                Cet argument a des sous-arguments :
+                - **join** concerne les messages de bienvenue;
+                - **leave** concerne les messages de départ.
+                Ces sous-arguments ont des sous-arguments :
+                - **reset** réinitialise le salon configuré, les messages de bienvenue/départ seront envoyés dans le salon par défaut;
+                - **this** définit le salon où a été envoyé cette commande;
+                - **identifiant/mention du salon** définit le salon en fonction de son identifiant ou de sa mention.
+                
+                - **prohibitword** :
+                Cet argument a des sous-arguments :
+                - **add** ajoute un mot à la liste des mots interdits;
+                - **remove** retire un mot à la liste des mots interdits;
+                - **reset** réinitialise la liste entière des mots interdits.
+                Les sous-arguments **add** et **remove** ont un sous-argument :
+                - **mot** est le mot qui sera ajouté ou retiré de la liste des mots interdits.
+                
+                - **localization** :
+                Cet argument a des sous-arguments :
+                - **en** définit la langue du bot du serveur en anglais;
+                - **fr** définit la langue du bot du serveur en français.
+                
+                - **joinrole** :
+                Cet argument a des sous-arguments :
+                - **reset** réinitialise le rôle configuré;
+                - **identifiant/mention du rôle** définit le salon en fonction de son identifiant ou de sa mention.
+                
+                - **setprefix** :
+                Cet argument a des sous-arguments :
+                - **reset** réinitialise le préfixe configuré, le préfixe utilisé sera `sb!`;
+                - **prefix** définit le préfixe spécifié.
+                """);
+        System.out.println("""
+                The available arguments are:
+                
+                - **channelmember**:
+                This argument has sub-arguments:
+                - **join** concerns the welcome messages;
+                - **leave** concerns the departure messages.
+                These sub-arguments have sub-arguments:
+                - **reset** resets the configured room, the welcome/departure messages will be sent in the default room;
+                - **this** defines the salon where this order was sent;
+                - **identifiant/mention du salon** defines the salon according to its identifier or mention.
+                
+                - **prohibitword**:
+                This argument has sub-arguments:
+                - **add** adds a word to the list of forbidden words;
+                - **remove** remove a word from the list of forbidden words;
+                - **reset** resets the entire list of forbidden words.
+                The **add** and **remove** sub-arguments have a sub-argument:
+                - **mot** is the word that will be added or removed from the list of forbidden words.
+                
+                - **localization**:
+                This argument has sub-arguments:
+                - **en** sets the server bot language to English;
+                - **fr** sets the server bot language to French.
+                
+                - **joinrole**:
+                This argument has sub-arguments:
+                - **reset** resets the configured role;
+                - **identifiant/mention du rôle** defines the salon according to its identifier or mention.
+                
+                - **setprefix**:
+                This argument has sub-arguments:
+                - **reset** resets the configured prefix, the prefix used will be `sb!`;
+                - **prefix** defines the specified prefix.
+                """);
         event.reply(stringBuilder.toString());
     }
 }
