7c7113b33080f1256d8c4c0a78f37069f9e52313
NoaLeGeek68 <noacraft68@gmail.com>
2022-03-12 11:20:18 +0100

    import json simple for RequestHelper

--- src/main/java/fr/noalegeek/pepite_dor_bot/utils/RequestHelper.java
+++ src/main/java/fr/noalegeek/pepite_dor_bot/utils/RequestHelper.java
@@ -6,7 +6,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 
 import java.io.IOException;
 
