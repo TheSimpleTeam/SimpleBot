57799cbd1ecc22cb75fdb7d6ac4b7d6d63a78b01
minemobs <issahakim94@gmail.com>
2022-04-16 22:04:05 +0200

    A little bit easier to read

--- src/main/java/net/thesimpleteam/simplebot/utils/UnicodeCharacters.java
+++ src/main/java/net/thesimpleteam/simplebot/utils/UnicodeCharacters.java
@@ -1,30 +1,5 @@
-/*
- * MIT License
- *
- * Copyright (c) 2021 minemobs
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package net.thesimpleteam.simplebot.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UnicodeCharacters {
@@ -42,21 +17,6 @@ public class UnicodeCharacters {
 
 
     public static List<Character> getAllExponentCharacters() {
-        List<Character> exponentCharactersList = new ArrayList<>();
-        exponentCharactersList.add('\u2070');
-        exponentCharactersList.add('\u00B9');
-        exponentCharactersList.add('\u00B2');
-        exponentCharactersList.add('\u00B3');
-        exponentCharactersList.add('\u2074');
-        exponentCharactersList.add('\u2075');
-        exponentCharactersList.add('\u2076');
-        exponentCharactersList.add('\u2077');
-        exponentCharactersList.add('\u2078');
-        exponentCharactersList.add('\u2079');
-        exponentCharactersList.add('\u207A');
-        exponentCharactersList.add('\u207B');
-        exponentCharactersList.add('\u207D');
-        exponentCharactersList.add('\u207E');
-        return exponentCharactersList;
+        return List.of('\u2070', '\u00B9', '\u00B2', '\u00B3', '\u2074', '\u2075', '\u2076', '\u2077', '\u2078', '\u2079', '\u207A', '\u207B', '\u207D', '\u207E');
     }
 }
